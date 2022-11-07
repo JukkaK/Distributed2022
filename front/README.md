f7c69c8c5dffd909653da1dca668d613c226dae7
Jari Sokka <sokkajar@gmail.com>
2022-11-07 13:26:56 +0200

    Starting point for the front

---
+++ front/README.md
@@ -0,0 +1,23 @@
+## Available Scripts
+
+In the project directory, you can run:
+
+## Install dependecies
+
+### `npm install`
+
+
+## JSON Serverin
+
+First start the JSON server
+
+### `npm run server`
+
+You can see the content from [http://localhost:3001/notes](http://localhost:3001/notes)
+
+## Front
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
---
+++ front/src/App.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+
+import Note from './components/Note'
+import noteService from './services/notes'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      id: notes.length + 1,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1> 
+      <ul>
+        {notes.map(note => 
+          <Note
+            key={note.id}
+            note={note}
+          />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App

db48fa9bbaf62303706763bab9a52ac6972d11d6
Jari Sokka <sokkajar@gmail.com>
2022-11-16 16:53:38 +0200

    json update

--- front/src/index.js
+++ front/src/index.js
@@ -1,11 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import axios from 'axios'
 
-axios.get('http://localhost:3001/products').then(response => {
-  const products = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App products={products} />
-  )
-})
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)

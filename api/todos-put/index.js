83eca1773de528cdd577fd7a89283cf7dde4c7e1
Jukka Koskelin <jukka.koskelin@zure.com>
2022-11-26 13:56:55 +0200

    just retain logging

--- api/todos-put/index.js
+++ api/todos-put/index.js
@@ -5,25 +5,28 @@ module.exports = async function (context, req) {
     context.log("MyEventGridTopicUriSetting: " + process.env["MyEventGridTopicUriSetting"]);
 
     if (req.body && req.body.task) {
-        
-        const { EventGridPublisherClient, AzureKeyCredential } = require("@azure/eventgrid");
+        context.res = {
+            status: 200,
+            body: todoService.editTodos(context)
+        };
+        // const { EventGridPublisherClient, AzureKeyCredential } = require("@azure/eventgrid");
 
-        const client = new EventGridPublisherClient(
-          process.env["MyEventGridTopicUriSetting"],
-          "EventGrid",
-          new AzureKeyCredential(process.env["MyEventGridTopicKeySetting"])
-        );
+        // const client = new EventGridPublisherClient(
+        //   process.env["MyEventGridTopicUriSetting"],
+        //   "EventGrid",
+        //   new AzureKeyCredential(process.env["MyEventGridTopicKeySetting"])
+        // );
         
-        await client.send([
-          {
-            eventType: "Azure.Sdk.SampleEvent",
-            subject: "Event Subject",
-            dataVersion: "1.0",
-            data: {
-              hello: "world"
-            }
-          }
-        ]);
+        // await client.send([
+        //   {
+        //     eventType: "Azure.Sdk.SampleEvent",
+        //     subject: "Event Subject",
+        //     dataVersion: "1.0",
+        //     data: {
+        //       hello: "world"
+        //     }
+        //   }
+        // ]);
 
     }
     else {

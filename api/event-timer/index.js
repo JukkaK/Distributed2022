2c70a5ce8785aa10551bd1ed0380cb9a48f8e3a4
Jukka Koskelin <jukka.koskelin@zure.com>
2022-11-26 12:59:42 +0200

    add timer function to spa

---
+++ api/event-timer/index.js
@@ -0,0 +1,12 @@
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+
+    context.bindings.outputEvent = {
+        id: 'message-id',
+        subject: 'subject-name',
+        dataVersion: '1.0',
+        eventType: 'event-type',
+        data: "event-data",
+        eventTime: timeStamp
+    };
+};

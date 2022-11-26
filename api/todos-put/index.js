35d871dc708418c0e4b6a43d4ff09bffc7049b13
Jukka Koskelin <jukka.koskelin@zure.com>
2022-11-26 13:31:47 +0200

    move eg output to todo-put function

--- api/todos-put/index.js
+++ api/todos-put/index.js
@@ -4,10 +4,23 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.body && req.body.task) {
+        
+        var timeStamp = new Date().toISOString();
+
+        context.bindings.outputEvent = {
+            id: 'message-id',
+            subject: 'subject-name',
+            dataVersion: '1.0',
+            eventType: 'event-type',
+            data: "event-data",
+            eventTime: timeStamp
+        };
+        
         context.res = {
             status: 200,
             body: todoService.editTodos(context)
         };
+
     }
     else {
         context.res = {

20c57e15857fa23497ca5661118f1f1bc3170038
Jukka Koskelin <jukka.koskelin@zure.com>
2022-11-26 14:18:23 +0200

    add eg sdk call back in

--- api/todos-put/index.js
+++ api/todos-put/index.js
@@ -5,28 +5,30 @@ module.exports = async function (context, req) {
     context.log("MyEventGridTopicUriSetting: " + process.env["MyEventGridTopicUriSetting"]);
 
     if (req.body && req.body.task) {
+
+        const { EventGridPublisherClient, AzureKeyCredential } = require("@azure/eventgrid");
+
+        const client = new EventGridPublisherClient(
+          process.env["MyEventGridTopicUriSetting"],
+          "EventGrid",
+          new AzureKeyCredential(process.env["MyEventGridTopicKeySetting"])
+        );
+        
+        await client.send([
+          {
+            eventType: "Azure.Sdk.SampleEvent",
+            subject: "Event Subject",
+            dataVersion: "1.0",
+            data: {
+              hello: "world"
+            }
+          }
+        ]);
+
         context.res = {
             status: 200,
             body: todoService.editTodos(context)
         };
-        // const { EventGridPublisherClient, AzureKeyCredential } = require("@azure/eventgrid");
-
-        // const client = new EventGridPublisherClient(
-        //   process.env["MyEventGridTopicUriSetting"],
-        //   "EventGrid",
-        //   new AzureKeyCredential(process.env["MyEventGridTopicKeySetting"])
-        // );
-        
-        // await client.send([
-        //   {
-        //     eventType: "Azure.Sdk.SampleEvent",
-        //     subject: "Event Subject",
-        //     dataVersion: "1.0",
-        //     data: {
-        //       hello: "world"
-        //     }
-        //   }
-        // ]);
 
     }
     else {

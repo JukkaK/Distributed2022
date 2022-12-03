760ca29b9813ed1529e265c3106dc45e38d37732
Jukka Koskelin <jukka.koskelin@zure.com>
2022-12-03 20:19:53 +0200

    add logging

--- serviceWorker/ServiceBusQueueTrigger1/index.ts
+++ serviceWorker/ServiceBusQueueTrigger1/index.ts
@@ -2,6 +2,7 @@ import { AzureFunction, Context } from "@azure/functions"
 
 const serviceBusQueueTrigger: AzureFunction = async function(context: Context, mySbMsg: any): Promise<void> {
     context.log('ServiceBus queue trigger function processed message', mySbMsg);
+    context.log("WEBSITE_SITE_NAME: " + process.env["WEBSITE_SITE_NAME"]); 
 };
 
 export default serviceBusQueueTrigger;

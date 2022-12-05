c452296af661786019656911245f8a985d406a41
Ville <vmuilu@gmail.com>
2022-12-05 20:54:34 +0200

    changed to axios

--- serviceWorker/ServiceBusQueueTrigger1/index.ts
+++ serviceWorker/ServiceBusQueueTrigger1/index.ts
@@ -1,14 +1,22 @@
 import { AzureFunction, Context } from "@azure/functions"
+import axios from 'axios';
 
-const serviceBusQueueTrigger: AzureFunction = async function(context: Context, mySbMsg: any): Promise<void> {
+const serviceBusQueueTrigger: AzureFunction = async function(context: Context, mySbMsg: any): Promise<Object> {
     context.log('ServiceBus queue trigger function processed message', mySbMsg);
     context.log("WEBSITE_SITE_NAME: " + process.env["WEBSITE_SITE_NAME"]); 
-    const response = await fetch('https://func-distributed-dbapi-we-001.azurewebsites.net/api/db', {
+
+        axios({
         method: 'PUT',
+        url:'https://func-distributed-dbapi-we-001.azurewebsites.net/api/db',
         headers: {'Content-Type': 'application/json --verbose'},
-        body:JSON.stringify({document: mySbMsg})
-        });
-    return await response.json();
-};
+        data: JSON.stringify({document: {"ean": mySbMsg.ean, "order":mySbMsg.amount}})
+        }).then(function (response) {
+            return response
+          });
+
+        return "nope"  
+    }
+    
+    
 
 export default serviceBusQueueTrigger;

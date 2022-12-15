72e820d21d5cedec4853436e48c78b4a86470484
Jukka Koskelin <jukka.koskelin@zure.com>
2022-12-15 21:41:52 +0200

    update event data

--- serviceWorker/ServiceBusQueueTrigger1/index.ts
+++ serviceWorker/ServiceBusQueueTrigger1/index.ts
@@ -15,18 +15,17 @@ const serviceBusQueueTrigger: AzureFunction = async function(context: Context, m
         data: {mySbMsg}
         }).then(function (response) {
             context.log("axios response: ", response.status);
-                var timeStamp = new Date().toISOString();
+                
                 context.bindings.outputEvent = {                    
                     dataVersion: '1.0',
                     eventType: 'update-state',
                     data: {
                         messageid: context.bindingData.messageId,
-                        time: timeStamp,
                         node: process.env["WEBSITE_SITE_NAME"] 
                     },
-                    eventTime: timeStamp                    
+                
                 }
-                context.log("sent event with timestamp: ", timeStamp);
+                context.log("sent event");
 
             return response
 

a7d98a7e503418bb82969bc62f7f8b356ccf8157
Jukka Koskelin <jukka.koskelin@zure.com>
2022-12-15 21:46:20 +0200

    just use example event data

--- serviceWorker/ServiceBusQueueTrigger1/index.ts
+++ serviceWorker/ServiceBusQueueTrigger1/index.ts
@@ -16,15 +16,13 @@ const serviceBusQueueTrigger: AzureFunction = async function(context: Context, m
         }).then(function (response) {
             context.log("axios response: ", response.status);
                 
-                context.bindings.outputEvent = {                    
-                    dataVersion: '1.0',
-                    eventType: 'update-state',
-                    data: {
-                        messageid: context.bindingData.messageId,
-                        node: process.env["WEBSITE_SITE_NAME"] 
-                    },
-                
-                }
+            context.bindings.outputEvent = {
+                id: 'message-id',
+                subject: 'subject-name',
+                dataVersion: '1.0',
+                eventType: 'event-type',
+                data: "event-data",                
+            };
                 context.log("sent event");
 
             return response

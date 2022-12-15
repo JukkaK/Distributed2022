b273e885b81a14673faee1ddbdd4a82392fdb9fc
Jukka Koskelin <jukka.koskelin@zure.com>
2022-12-15 22:08:32 +0200

    Add table binding and code

--- serviceWorker/EventGridTrigger/index.ts
+++ serviceWorker/EventGridTrigger/index.ts
@@ -3,6 +3,17 @@ import { AzureFunction, Context } from "@azure/functions"
 const eventGridTrigger: AzureFunction = async function (context: Context, eventGridEvent: any): Promise<void> {
     context.log(typeof eventGridEvent);
     context.log(eventGridEvent);
+
+    context.bindings.tableBinding = [];
+
+    for (var i = 1; i < 10; i++) {
+        context.bindings.tableBinding.push({
+            PartitionKey: "Test",
+            RowKey: i.toString(),
+            Name: "Name " + i
+        });
+    }
+
 };
 
 export default eventGridTrigger;

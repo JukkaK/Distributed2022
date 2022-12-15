6b6226586451a6f0557efbaa6654312a271b4e80
Jukka Koskelin <jukka.koskelin@zure.com>
2022-12-15 22:16:04 +0200

    simplify table push

--- serviceWorker/EventGridTrigger/index.ts
+++ serviceWorker/EventGridTrigger/index.ts
@@ -6,13 +6,11 @@ const eventGridTrigger: AzureFunction = async function (context: Context, eventG
 
     context.bindings.tableBinding = [];
 
-    for (var i = 1; i < 10; i++) {
-        context.bindings.tableBinding.push({
-            PartitionKey: "Test",
-            RowKey: i.toString(),
-            Name: "Name " + i
-        });
-    }
+    context.bindings.tableBinding.push({
+        PartitionKey: "Test",
+        RowKey: "1",
+        Name: "Name"
+    });    
 
 };
 
